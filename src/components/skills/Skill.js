0918a08ba4d4177892daccf4ff3fe06eb3279939
Prajwal <61734018+prajwalr308@users.noreply.github.com>
2021-04-03 19:44:27 +0530

    skills and exp

--- src/components/skills/Skill.js
+++ src/components/skills/Skill.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Skill.css'
 
 const Skill = () => {
+   const [state, setstate] = useState({
+       'skillSelected':false,
+       'expSelected':false,
+   })
+    const skillHandler=()=>{
+        setstate({
+            'skillSelected':true,
+       'expSelected':false,
+        })
+    }
+    const expHandler=()=>{
+        setstate({
+            'skillSelected':false,
+       'expSelected':true,
+        })
+    }
     return (
         <div className="skill">
             <div className="skill__buttons">
-            <button>skills</button>
-            <button>Experience</button>
+            <button className={state.skillSelected ? "skill__buttons__skills--selected": "skill__buttons__skills"} onClick={skillHandler} >skills</button>
+            
+            <button className={state.expSelected ? "skill__buttons__exp--selected":"skill__buttons__exp"} onClick={expHandler}>Experience</button>
             </div>
             <div></div>
             
