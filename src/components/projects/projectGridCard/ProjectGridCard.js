d195193e1a25557a7dbd97814334ff1dc2244477
prajwalr308 <prajwalr.thedeveloper@gmail.com>
2021-04-08 17:46:53 +0530

    project directory

---
+++ src/components/projects/projectGridCard/ProjectGridCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ProjectGridCard = () => {
+    return (
+        <div>
+            <h2>Card</h2>
+
+<div className="card">
+  <img src="img_avatar.png" alt="Avatar" style="width:100%" />
+  <div className="container">
+    <h4><b>John Doe</b></h4> 
+    <p>Architect & Engineer</p> 
+  </div>
+</div>
+        </div>
+    )
+}
+
+export default ProjectGridCard
