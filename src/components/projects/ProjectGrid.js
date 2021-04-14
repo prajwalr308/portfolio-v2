11dd73f37774cdacbd19a267f6f55190331d1b65
Prajwal <61734018+prajwalr308@users.noreply.github.com>
2021-04-14 18:34:12 +0530

    project grid animations

--- src/components/projects/ProjectGrid.js
+++ src/components/projects/ProjectGrid.js
@@ -5,20 +5,15 @@ import './ProjectGrid.css'
 const ProjectGrid = () => {
     return (
         <div className="ProjectGrid">
-            <h2>Projects</h2>
-            <div className="ProjectGrid__flex">
+            <div className="ProjectGrid__grid">
             <ProjectGridCard />
-            <ProjectGridCard />
-            <ProjectGridCard />
-
+          <ProjectGridCard />
+          <ProjectGridCard />
+          <ProjectGridCard />
+          <ProjectGridCard />
+          <ProjectGridCard />
             </div>
-            
-            <div className="ProjectGrid__flex">
-            <ProjectGridCard />
-            <ProjectGridCard />
-            <ProjectGridCard />
-
-            </div>
+         
         </div>
     )
 }
--- src/components/projects/projectGridCard/ProjectGridCard.js
+++ src/components/projects/projectGridCard/ProjectGridCard.js
@@ -4,19 +4,25 @@ import code from '../../../assets/coding.svg'
 
 const ProjectGridCard = () => {
   return (
-    <div>
-   
-
-      <div className="card">
-        <img src={code} alt="Avatar" style={{width: "100%"}} />
-        <div className="card__container">
-          <h4>
-            <b>John Doe</b>
-          </h4>
-          <p>Architect & Engineer</p>
+    <div className="container1">
+<div className="card">
+            <div className="face face1">
+                <div className="content">
+                    <div className="icon">
+                        <img className="fa fa-linkedin-square" src={code} />
+                    </div>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                    <h3>
+                        <a href="https://www.linkedin.com/in/adamdipinto/" target="_blank">_adamdipinto</a>
+                    </h3>
+                    <p>This is where I network and build my professional protfolio.</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
+</div>
   );
 };
 
