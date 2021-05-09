eb56b6b31c710d2a2b06b85a9c76d607513d2016
prajwalr308 <prajwalr.thedeveloper@gmail.com>
2021-05-09 21:05:45 +0530

    projects update

--- src/components/projects/ProjectGrid.js
+++ src/components/projects/ProjectGrid.js
@@ -7,6 +7,7 @@ import github from '../../assets/Image 1.png'
 import ui from '../../assets/Image 2.png'
 import Restaurant from '../../assets/Restaurant.png'
 import amazon from '../../assets/amazon.png'
+import insta from '../../assets/insta.png'
 
 
 const ProjectGrid = () => {
@@ -15,12 +16,13 @@ const ProjectGrid = () => {
             <div className="ProjectGrid__titleContainer"><h1 className="ProjectGrid__heading">Projects</h1></div>
             
             <div className="ProjectGrid__grid">
-            <ProjectGridCard body="twitter clone built using react" title="Twitter" img={Twitter} link="https://twitter-clone-8d64a.web.app/" />
-          <ProjectGridCard body="github api project using react" title="github api"  img={github} link="https://github-react-api.netlify.app" />
+            <ProjectGridCard body="instagram clone built using react and firebase" link="https://instagram-clone-64f1c.web.app/" title="Instagram Clone" img={insta}  />
+            <ProjectGridCard body="twitter clone built using react and firebase" title="Twitter" img={Twitter} link="https://twitter-clone-8d64a.web.app/" />
+          <ProjectGridCard body="github api project using react and github api" title="github api"  img={github} link="https://github-react-api.netlify.app" />
           <ProjectGridCard body="restaurant api project using react" title="Restaurant" img={Restaurant} link="https://prajwalr308.github.io/RestaurantFinder/" />
           <ProjectGridCard body="ui design" title="ui" img={ui} link="https://freshgroceryreact.netlify.app" />
           <ProjectGridCard body="amazon clone built using mern stack"  title="amazon"  img={amazon} link="https://reactamazonclone.herokuapp.com/" />
-          <ProjectGridCard body="twitter clone built using react" title="Twitter" img={Twitter} link="" />
+          
             </div>
          
         </div>
